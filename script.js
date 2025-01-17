986bbdee09ed617658378d48e5fd91e9e0832802
Sundar <164506011+Sundxr2005@users.noreply.github.com>
2025-01-18 00:28:20 +0530

    FRONTEND CHANGES

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function openLoginModal() {
     document.getElementById('loginModal').style.display = 'flex';
 }
-
 function openRegisterModal() {
     closeAllModals();
     document.getElementById('registerModal').style.display = 'flex';
 }
-
 function closeAllModals() {
     document.querySelectorAll('.modal').forEach(modal => {
         modal.style.display = 'none';
     });
 }
-
 window.onclick = function(event) {
     const modals = document.querySelectorAll('.modal');
     modals.forEach(modal => {
