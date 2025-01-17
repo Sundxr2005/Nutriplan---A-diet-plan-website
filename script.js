971d49b913bcc43550fab803806711b99f2815b2
Sundar <164506011+Sundxr2005@users.noreply.github.com>
2025-01-17 20:29:54 +0530

    CHANGES

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function openLoginModal() {
     document.getElementById('loginModal').style.display = 'flex';
 }
+
 function openRegisterModal() {
-    closeAllModals(); 
+    closeAllModals();
     document.getElementById('registerModal').style.display = 'flex';
 }
+
 function closeAllModals() {
     document.querySelectorAll('.modal').forEach(modal => {
         modal.style.display = 'none';
     });
 }
+
 window.onclick = function(event) {
     const modals = document.querySelectorAll('.modal');
     modals.forEach(modal => {
